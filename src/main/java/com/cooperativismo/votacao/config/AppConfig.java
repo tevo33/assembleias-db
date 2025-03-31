ed9d8b7fcea2662969c1b4215768f9a52531f089
tevo33 <tevoguerra@gmail.com>
2025-03-30 22:00:41 -0300

    Adição dos arquivos de config

---
+++ src/main/java/com/cooperativismo/votacao/config/AppConfig.java
@@ -0,0 +1,17 @@
+package com.cooperativismo.votacao.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@EnableConfigurationProperties(CallbackConfig.class)
+public class AppConfig {
+    
+    @Bean
+    public RestTemplate restTemplate()
+    {
+        return new RestTemplate();
+    }
+} 
